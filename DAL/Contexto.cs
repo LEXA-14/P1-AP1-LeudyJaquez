90a4769d53704d31c2c25aadefb82a4436df6e15
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2025-09-22 19:25:40 -0400

    se creo contexto (placeholder registro)

---
+++ DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using P1_AP1_LeudyJaquez.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace P1_AP1_LeudyJaquez.DAL;
+
+public class Contexto: DbContext
+{
+    public Contexto(DbContextOptions options) : base(options)
+    { }
+
+    public DbSet<Contexto> Registro { get; set; }
+
+    //registro is a nameplaceholder
+
+   
+}
