616143c9011a78376e1267e6d2e61de1a376145b
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2025-09-22 19:17:37 -0400

    se creo placeholder de carperta model Registro

---
+++ Model/Registro.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P1_AP1_LeudyJaquez.Model;
+
+    public class Registro
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage ="Este campo no puede estar vacio")]
+
+    public string Nombres { get; set; }
+}
+
