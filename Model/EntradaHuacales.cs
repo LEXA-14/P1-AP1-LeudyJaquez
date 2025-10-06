21245a2e41dd1ab9f4095fe56eeb5c1501929249
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2025-10-05 21:33:28 -0400

    se creo campo importe y se agregaron las validaciones a campos cantidad y precio

--- Model/EntradaHuacales.cs
+++ Model/EntradaHuacales.cs
@@ -8,23 +8,26 @@ public class EntradaHuacales
     [Key]
     public int IdEntrada { get; set; }
 
+    
     [Required(ErrorMessage = "Este campo no puede estar vacio")]
 
     public string NombreCliente { get; set; }
 
     [Required(ErrorMessage = "Este campo no puede estar vacio")]
 
-    public DateTime Fecha { get; set; } 
+    public DateTime Fecha { get; set; } = DateTime.Now;
 
     [Required(ErrorMessage ="Este campo no puede estar vacio")]
 
+    [Range(0, int.MaxValue, ErrorMessage = "La Cantidad no puede ser negativa")]
     public int Cantidad { get; set; }
 
     [Required(ErrorMessage = "Este campo no puede estar vacio")]
 
+    [Range(0,int.MaxValue,ErrorMessage ="El Precio no puede ser negativo")]
     public double Precio { get; set; }
 
-   
+    public double Importe => Cantidad * Precio;
 
 
 
